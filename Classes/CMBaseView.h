eed720c4d923ae9dccaa25f407d08c3019d8f370
唐亚东 <tangyd@g-bits.com>
2023-03-28 14:52:32 +0800

    初始化项目

---
+++ Classes/CMBaseView.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CMBaseView : UIView
+/**
+  页面初始化方法
+ */
+- (void)initView;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/CMBaseView.m
@@ -0,0 +1,29 @@
+#import "CMBaseView.h"
+
+@implementation CMBaseView
+
+#pragma mark - 初始化
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    self.clipsToBounds = YES;
+    [self initView];
+    
+}
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.clipsToBounds = YES;
+        [self initView];
+    }
+    return self;
+}
+
+- (void)initView {
+    /*
+     让子类去实现
+     */
+}
+
+@end
